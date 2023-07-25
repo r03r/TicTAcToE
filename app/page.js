e0e507417ed43f00eacfc04b89df4b43ab161c37
edRRR <r03r000@gmail.com>
2023-07-25 17:08:09 +0200

    stable funtion  commit

--- app/page.js
+++ app/page.js
@@ -23,17 +23,19 @@ function Square() {
 }
 
 export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
   return (
     <div className={"grid grid-cols-3 gap-1 max-w-2xl "}>
-      <Square value="1"/>
-      <Square value="2"/>
-      <Square value="3"/>
-      <Square value="1"/>
-      <Square value="1"/>
-      <Square value="1"/>
-      <Square value="1"/>
-      <Square value="1"/>
-      <Square value="1"/>
+      <Square value={squares[0]}/>
+      <Square value={squares[1]}/>
+      <Square value={squares[2]}/>
+      <Square value={squares[3]}/>
+      <Square value={squares[4]}/>
+      <Square value={squares[5]}/>
+      <Square value={squares[6]}/>
+      <Square value={squares[7]}/>
+      <Square value={squares[8]}/>
     </div>
   );
 }
