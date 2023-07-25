91d374192f00b23ffaea11b7127362c46e13d465
edRRR <r03r000@gmail.com>
2023-07-25 22:12:18 +0200

    ~  git commit 'cambio de jugador cambia turno y no se puede repetir casilla (fija el elemento marcado)'

--- app/page.js
+++ app/page.js
@@ -1,40 +1,56 @@
-  'use client'
+"use client";
 
 import { useState } from "react";
 
-function Square({value, onSquareClick}) {
+function Square({ value, cuandoHagaClick }) {
   //const [value, setValue] = useState(null);
   return (
     <button
-      className={"bg-blue-500 py-2 px-2 rounded-lg  hover:bg-blue-700 text-white font-bold"}onClick={onSquareClick}>
+      className={
+        "bg-blue-500 py-2 px-2 rounded-lg  hover:bg-blue-700 text-white font-bold"
+      }
+      onClick={cuandoHagaClick}
+    >
       {value}
     </button>
   );
 }
-// esto es un comentario para git 
+// esto es un comentario para git
 export default function Board() {
+  const [xIsNext, setXIsNext] = useState(true);
+
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handledClick(i) {
+    //cambio entre las dos figuras para escojer casilla y escojer cambio de turno  
+    if (squares[i]) {
+      return
+    }
     const nextSquare = squares.slice();
-    nextSquare[i] = '🐼';
-    setSquares(nextSquare)
 
-   // setValue('🐼');
-   // setValue('🕷');
+    if (xIsNext) {
+      nextSquare[i] = "🐼";
+    } else {
+      nextSquare[i] = "🕷";
+    }
+    setSquares(nextSquare);
+    setXIsNext(!xIsNext);
+
+    // setValue('🐼');
+    // setValue('🕷');
   }
 
   return (
     <div className={"grid grid-cols-3 gap-1 max-w-2xl"}>
-      <Square value={squares[0]}onSquareClick={()=>handledClick(0)}/>
-      <Square value={squares[1]}onSquareClick={()=>handledClick(1)}/>
-      <Square value={squares[2]}onSquareClick={()=>handledClick(2)}/>
-      <Square value={squares[3]}onSquareClick={()=>handledClick(2)}/>
-      <Square value={squares[4]}onSquareClick={()=>handledClick(4)}/>
-      <Square value={squares[5]}onSquareClick={()=>handledClick(5)}/>
-      <Square value={squares[6]}onSquareClick={()=>handledClick(6)}/>
-      <Square value={squares[7]}onSquareClick={()=>handledClick(7)}/>
-      <Square value={squares[8]}onSquareClick={()=>handledClick(8)}/>
+      <Square value={squares[0]} cuandoHagaClick={() => handledClick(0)} />
+      <Square value={squares[1]} cuandoHagaClick={() => handledClick(1)} />
+      <Square value={squares[2]} cuandoHagaClick={() => handledClick(2)} />
+      <Square value={squares[3]} cuandoHagaClick={() => handledClick(3)} />
+      <Square value={squares[4]} cuandoHagaClick={() => handledClick(4)} />
+      <Square value={squares[5]} cuandoHagaClick={() => handledClick(5)} />
+      <Square value={squares[6]} cuandoHagaClick={() => handledClick(6)} />
+      <Square value={squares[7]} cuandoHagaClick={() => handledClick(7)} />
+      <Square value={squares[8]} cuandoHagaClick={() => handledClick(8)} />
     </div>
   );
 }
